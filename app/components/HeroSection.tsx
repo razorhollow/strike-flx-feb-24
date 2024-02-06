bd506a76d12d49efce481a1d1c84607bcebe6a9e
RazorHollow <rob@razorhollow.com>
2024-02-06 15:35:18 -0500

    fix broken register link in hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -138,7 +138,7 @@ export default function HeroSection() {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                to="#"
+                to="#register-section"
                 className="w-1/2 rounded-md bg-burn px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-straw focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
                 Register
