71d987395a916973041c9966ef9a469b8227b782
razorhollow <rob@razorhollow.com>
2024-02-06 17:49:42 -0500

    add comments to attendee list

--- app/components/Attendees.tsx
+++ app/components/Attendees.tsx
@@ -17,7 +17,7 @@ interface AttendeesProps {
 
 export default function Attendees({ attendees }: AttendeesProps) {
   return (
-    <div className="w-3/4 mx-auto">
+    <div className="sm:w-100 sm:mx-auto md:w-3/4 mx-auto">
       <ul className="divide-y divide-gray-100">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Current RSVP List</h2>
         {attendees.map((person) => (
@@ -25,7 +25,8 @@ export default function Attendees({ attendees }: AttendeesProps) {
             <div className="flex min-w-0 gap-x-4">
               <div className="min-w-0 ">
                 <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">registered on {moment(person.createdAt).format("MMMM Do YYYY")}</p>
+                <p className="mt-1 text-xs leading-5 text-gray-500">registered on {moment(person.createdAt).format("MMMM Do YYYY")}</p>
+                <p className="mt-1 text-xs leading-5 text-gray-500">{person.comments}</p>
               </div>
               <DialogDemo />
             </div>
