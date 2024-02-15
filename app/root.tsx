89a04756aea31734e211b0d0a630f3c31f2d4622
RazorHollow <rob@razorhollow.com>
2024-02-15 13:55:18 -0500

    update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -10,12 +10,16 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import faviconAppleTouchUrl from 'app/assets/apple-touch-icon.png'
+import faviconAssetUrl from 'app/assets/favicon.ico'
 import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "icon", sizes: "180x180", href: faviconAppleTouchUrl},
+  { rel: "icon", href: faviconAssetUrl}
 ];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
