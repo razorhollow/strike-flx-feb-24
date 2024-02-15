2ce12684b1abcd97be6d209e4292fe052bbdf96a
RazorHollow <rob@razorhollow.com>
2024-02-15 11:41:46 -0500

    adjust spacing in rsvp list items

--- app/components/Attendees.tsx
+++ app/components/Attendees.tsx
@@ -21,14 +21,16 @@ export default function Attendees({ attendees }: AttendeesProps) {
       <ul className="divide-y divide-gray-100">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Current RSVP List</h2>
         {attendees.map((person) => (
-          <li key={person.id} className="flex gap-x-4 py-5">
-            <div className="flex justify-between min-w-0 gap-x-4">
+          <li key={person.id} className="flex gap-x-4 py-5 w-full">
+            <div className="flex justify-between w-full gap-x-4">
               <div className="min-w-0 ">
                 <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
                 <p className="mt-1 text-xs leading-5 text-gray-500">registered on {moment(person.createdAt).format("MMMM Do YYYY")}</p>
                 <p className="mt-1 text-xs leading-5 text-gray-500">{person.comments}</p>
               </div>
-              <DialogDemo />
+              <div>
+                <DialogDemo />
+              </div>
             </div>
           </li>
         ))}
